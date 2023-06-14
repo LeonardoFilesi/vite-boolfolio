1662be7e363a169080c7b3064d8eda93dd81a0fe
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-06-14 19:11:25 +0200

    feat: :sparkles: Adds: most of the exercise from yesterday

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,36 @@
+<script>
+import { store } from "../store";
+
+export default {
+    name: 'ProjectCard',
+    data() {
+        return {
+            store
+        }
+    },
+    props: {
+        project: Object
+    },
+    computed: {
+        contentPreview() {
+            //controlla appunti Olga
+        }
+    }
+}
+</script>
+
+<template>
+
+        <div class="card h-90">
+            <img v-if="project.image" src="${store.apiBaseUrl}/storage/${project.image}" alt="">
+            <div class="card-body">
+                <h5>{{ project.title }}</h5>
+                <!-- <p>{{ contentPreview }}</p> -->
+            </div>
+        </div>
+    
+</template>
+
+<style lang="scss" scoped>
+
+</style>
