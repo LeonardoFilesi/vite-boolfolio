27ea107618c5b1ae18f73a721ac444f9773313d1
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-06-15 20:02:34 +0200

    feat: asdf

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import { createRouter } from "vue-router";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomePage,
+        },
+        {
+            path: "/projects",
+            name: "projects",
+            component: ProjectsPage,
+        }
+    ]
+});
